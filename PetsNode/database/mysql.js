450d88bbdda147adeeecc6cce13222a1bdbfa045
EdvardasMarozas <edvardas.marozas@stud.ktmc.lt>
2024-04-04 16:02:14 +0300

    added env variables

--- PetsNode/database/mysql.js
+++ PetsNode/database/mysql.js
@@ -5,10 +5,10 @@ const db = mysql.createPool({
   waitForConnections: true,
   queueLimit: 0,
 
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "petswar",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = db;
